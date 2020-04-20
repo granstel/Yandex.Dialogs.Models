be161e5408e99369fc6571ed5e505b9e4e174ac2
Stepan Grankin <sgrankin@inbox.ru>
2020-04-20 23:30:38 +0300

    Convert enum value to string

--- src/Cards/CardType.cs
+++ src/Cards/CardType.cs
@@ -1,5 +1,9 @@
-﻿namespace Yandex.Dialogs.Models.Cards
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Yandex.Dialogs.Models.Cards
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum CardType
     {
         BigImage,
--- src/Entity.cs
+++ src/Entity.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace Yandex.Dialogs.Models
