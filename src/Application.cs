6abfd70d40c3edb256d6325995ad7080793aa93c
Stepan Grankin <sgrankin@inbox.ru>
2021-01-24 20:51:23 +0300

    fix

--- src/Application.cs
+++ src/Application.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json.Serialization;
 namespace Yandex.Dialogs.Models
 {
     [JsonObject(NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
-    public class Application    
+    public class Application
     {
         public string ApplicationId { get; set; }
     }
